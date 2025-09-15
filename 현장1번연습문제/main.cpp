e521c1d28bb56c59fa67e655d9c6ec9ca978d142
YeDam-Cho <haivro1256@gmail.com>
2025-09-15 16:42:46 +0900

    2021010528 조예닮 #1

--- 현장1번연습문제/main.cpp
+++ 현장1번연습문제/main.cpp
@@ -6,7 +6,7 @@ int main()
 	printf("Name\t: %s\n\n","Yedam");
 	printf("Initial\t: %s\n\n","Y");
 	printf("Age : 24\n\n");
-	printf("Score\t: 93%.693.500000\n\n");
+	printf("Score\t: 93.500000\n\n");
 	printf("Count\t: 3000000000\n\n");
 	return 0;
 
