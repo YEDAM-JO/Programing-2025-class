dbf70ecc88c95d5f38b27e563132615de45a41f8
YeDam-Cho <haivro1256@gmail.com>
2025-09-15 16:13:41 +0900

    2021010528 조예닮 #1

---
+++ 현장1번연습문제/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("=== Basic Constants===\n");
+	printf("Name\t: Yedam\n");
+	printf("Initial\t: Y\n");
+	printf("Age : 24\n");
+	printf("Score\t: 93%.693.500000\n");
+	printf("Count\t: 3000000000\n");
+	return 0;
+
+}
