1fd1cad5953e02253d4ed3f588fd84f1038d1103
Yedam-Cho <haivro1256@gmail.com>
2025-10-01 23:11:26 +0900

    10월1일실습결과제출

---
+++ 10월1일 실습내용 제출/main.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main(void)
+{
+    char name[20];
+    char studentID[20];
+    double height;
+    double weight;
+    double bmi;
+
+    printf("Name, StudentID, Height(cm), weight(kg) \n\n");
+    scanf_s("%s %s %lf %lf", name, (unsigned)sizeof(name), studentID, (unsigned)sizeof(studentID), &height, &weight);
+
+    bmi = weight / ((height / 100.0) * (height / 100.0));
+
+
+    printf("Name : %s\n\n", name);
+    printf("StudentID : %s\n\n", studentID);
+    printf("Height(cm) : %.1lf cm\n\n", height);
+    printf("weight(kg) : %.1lf kg\n\n", weight);
+
+ 
+    printf("BMI: %.2lf\n", bmi);
+
+    return 0;
+}
--- Helloworld4/main.cpp
+++ Helloworld4/main.cpp
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	printf("Hello, World!Å_n");
+	char code1 = 'A';
+	const char* message = "Fisrt line\n\n\tIndented second line\n\nThird line";
+	const char* quotes = "\"double quotes\" and 'single quotes'";
+	float average = 87.35f;
+
+	printf("=== Char/string & Escapes ===\n\n");
+	printf("Char : %c, code: %d\n\n", code1, code1);
+
+	printf("Message:\n\n");
+	printf("%s\n\n", message);
+
+	printf("Quotes:%s\n\n", quotes);
+	printf("Backslash: \\\n\n");
+
+	printf("Average:[%8.2f]", average);
+
 	return 0;
-}
+
